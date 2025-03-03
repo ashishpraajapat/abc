1357b864d6f9cfb19be11dd97ea4ca36aa75d27f
ashishpraajapat <ap162252@gmail.com>
2025-03-03 23:15:08 +0530

    updated changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import {app} from "./app.js";
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8089, ()=>{
+        console.log(`App is listing on ${process.env.PORT} !!!`);
+        
+    })
+}
+).catch((error)=> console.error("MONGODB CONNECTION ERR !!: ",error)
+)
 
 
 
